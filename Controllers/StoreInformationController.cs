d8e1116c0c9510605997ea86132e74396be8358c
LiaoTingWei1 <liao990426@gmail.com>
2025-03-11 14:00:51 +0800

    新增門市資訊頁面及訂位表單.v01

---
+++ Controllers/StoreInformationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Models;
+
+namespace Restaurant.Controllers
+{
+    public class StoreInformationController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public StoreInformationController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> StoreInformation()
+        {
+            var stores = await _context.RestaurantInfos.ToListAsync();
+            return View(stores);
+        }
+        //public IActionResult StoreInformation()
+        //{
+        //    return View();
+        //}
+    }
+}
