9270c74d7f0892dbfe278508c22a81670f1c6685
ddg8877tw <ddg8877tw@yahoo.com.tw>
2025-03-31 00:29:32 +0800

    新增菜單維護頁面、餐點圖，更改公版、css、index

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,48 +1,164 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Restaurant.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace Restaurant.Controllers
 {
     public class MenuController : Controller
     {
-        private readonly ILogger<MenuController> _logger;
         private readonly MyDbContext _context;
 
-        public MenuController(ILogger<MenuController> logger, MyDbContext context)
+        public MenuController(MyDbContext context)
         {
-            _logger = logger;
             _context = context;
         }
 
         [AllowAnonymous]
         public async Task<IActionResult> Index_Musteat()
         {
-            var menus = await _context.Menus.ToListAsync();
+            var menus = await _context.Menus
+                .Where(e => e.MenuIsAvailable) // 過濾只顯示可用的菜單項目
+                .ToListAsync();
             return View(menus);
         }
 
         [AllowAnonymous]
         public async Task<IActionResult> Index1_Mealsincluded()
         {
-            var menus = await _context.Menus.ToListAsync();
+            var menus = await _context.Menus
+                 .Where(e => e.MenuIsAvailable) // 過濾只顯示可用的菜單項目
+                 .ToListAsync();
             return View(menus);
         }
 
         [AllowAnonymous]
         public async Task<IActionResult> Index2_Desserts()
         {
-            var menus = await _context.Menus.ToListAsync();
+            var menus = await _context.Menus
+                .Where(e => e.MenuIsAvailable) // 過濾只顯示可用的菜單項目
+                .ToListAsync();
             return View(menus);
         }
 
-        //public async Task<IActionResult> Index3_Menuadmin()
-        //{
-        //    var menus = await _context.Menus.ToListAsync();
-        //    return View(menus);
-        //}
+        // 顯示所有菜單
+        public async Task<IActionResult> Index3_MenuRead()
+        {
+            var menus = await _context.Menus.OrderByDescending(e => e.MenuId).ToListAsync();
+            return View(menus);
+        }
+
+        // 顯示新增表單 (GET)
+        public IActionResult Index4_MenuCreate()
+        {
+            return View();
+        }
+
+        // 處理表單提交  (POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index4_MenuCreate(MenuView menu)
+        {
+            // 移除 ModelState 中的 MenuCategory 錯誤
+            ModelState.Remove(nameof(menu.MenuCategory));
+
+            if (ModelState.IsValid)
+            {
+                // 根據類別編號設置類別名稱
+                switch (menu.MenuCategoryId)
+                {
+                    case 1:
+                        menu.MenuCategory = "火鍋";
+                        break;
+                    case 2:
+                        menu.MenuCategory = "附食";
+                        break;
+                    case 3:
+                        menu.MenuCategory = "甜點";
+                        break;
+                }
+
+                _context.Menus.Add(menu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index3_MenuRead));  // 新增完成後跳轉回菜單列表
+            }
+
+            return View(menu);
+        }
+
+        // 顯示編輯表單(GET)
+        public async Task<IActionResult> Index5_MenuEdit(int id)
+        {
+            var menu = await _context.Menus.FirstOrDefaultAsync(e => e.MenuId == id); // 根據 ID 獲取菜單數據
+            if (menu == null)
+            {
+                return NotFound(); // 如果菜單不存在，返回 404
+            }
+
+            // 創建 MenuView 模型並將數據填充
+            var model = new MenuView
+            {
+                MenuId = menu.MenuId,
+                MenuCategory = menu.MenuCategory,
+                MenuCategoryId = menu.MenuCategoryId,
+                MenuName = menu.MenuName,
+                MenuDescription = menu.MenuDescription,
+                MenuPrice = menu.MenuPrice,
+                MenuIsAvailable = menu.MenuIsAvailable,
+                MenuImageUrl = menu.MenuImageUrl
+            };
+
+            return View(model); // 傳遞模型到視圖
+        }
+
+        // 處理表單提交(POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index5_MenuEdit(MenuView menu)
+        {
+            // 移除 ModelState 中的 MenuCategory 錯誤
+            ModelState.Remove(nameof(menu.MenuCategory));
+
+            if (ModelState.IsValid)
+            {
+                var menuInDb = await _context.Menus.FirstOrDefaultAsync(e => e.MenuId == menu.MenuId);
+                if (menuInDb == null)
+                {
+                    return NotFound();
+                }
+
+                // 根據類別編號設置類別名稱
+                switch (menu.MenuCategoryId)
+                {
+                    case 1:
+                        menuInDb.MenuCategory = "火鍋";
+                        break;
+                    case 2:
+                        menuInDb.MenuCategory = "附食";
+                        break;
+                    case 3:
+                        menuInDb.MenuCategory = "甜點";
+                        break;
+                }
+                menuInDb.MenuCategoryId = menu.MenuCategoryId;
+                menuInDb.MenuName = menu.MenuName;
+                menuInDb.MenuDescription = menu.MenuDescription;
+                menuInDb.MenuPrice = menu.MenuPrice;
+                menuInDb.MenuIsAvailable = menu.MenuIsAvailable;
+                menuInDb.MenuImageUrl = menu.MenuImageUrl;
+
+                _context.Menus.Update(menuInDb);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index3_MenuRead));  // 更新完成後跳轉回菜單列表
+            }
+
+            return View(menu);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Models/MenuView.cs
+++ Models/MenuView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,33 +8,32 @@ namespace Restaurant.Models;
 
 public partial class MenuView
 {
+    [Display(Name = "編號")]
     public int MenuId { get; set; }
 
-    [Display(Name = "菜品種類")]
-    public int MenuCategoryId { get; set; }
-
+    [Display(Name = "類別")]
     public string MenuCategory { get; set; } = null!;
 
-    [Display(Name = "菜品名稱")]
+    [Display(Name = "名稱")]
     public string MenuName { get; set; } = null!;
 
-    [Display(Name = "菜品描述")]
+    [Display(Name = "描述")]
     public string? MenuDescription { get; set; }
 
-    [Display(Name = "菜品價格")]
+    [Display(Name = "價格")]
     public int MenuPrice { get; set; }
 
+    [Display(Name = "狀態")]
     public bool MenuIsAvailable { get; set; }
 
-    [Display(Name = "菜品是否供應")]
+    [Display(Name = "類別編號")]
+    public int MenuCategoryId { get; set; }
+
     [NotMapped]
     public string? MenuAvailable { get; set; }
 
-    [Display(Name = "菜單圖片連結")]
+    [Display(Name = "圖片 / 路徑")]
     public string? MenuImageUrl { get; set; }
-
-    [NotMapped]
-    public IFormFile? MenuImageFile { get; set; }  // 用來接收上傳的檔案
 
     public virtual ICollection<OrderItemView> OrderItems { get; set; } = new List<OrderItemView>();
 }
